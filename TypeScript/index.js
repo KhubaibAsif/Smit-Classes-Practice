de763c7699c2a735a2968747c948bc48ff396e01
Khubaiblakhpat <153702821+KhubaibAsif@users.noreply.github.com>
2024-07-20 00:10:19 +0500

    Learning TypeScript

---
+++ TypeScript/index.js
@@ -0,0 +1,26 @@
+let userName: string = "Khubaib Asif";
+let userAge: number = 28;
+let userFavDishes: string[] = ["Biryani", "Pizza"];
+let userIsMarried: boolean = false;
+
+console.log("userName=>", userName);
+console.log("userAge=>", userAge);
+
+userIsMarried = false;
+
+
+const user: User = {
+  firstName: "Khubaib",
+  lastName: "Asif",
+  favNumbers: [10, 12],
+  favSubjects: ["English", "Urdu"],
+  age: 20,
+};
+
+function greet(name: string): string {
+    return `Hello ${name}`;
+  }
+  
+  let msg : string =  greet("Khubaib Asif");
+  
+  console.log(msg)
