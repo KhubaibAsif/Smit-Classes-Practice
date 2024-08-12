e9eda5681bb020570f4c0c62579e8658708c312f
Khubaiblakhpat <153702821+KhubaibAsif@users.noreply.github.com>
2024-08-12 07:19:59 +0500

    Update app.js

--- Password-Generator/app.js
+++ Password-Generator/app.js
@@ -10,27 +10,22 @@ function generatePassword() {
     const smalls = 'abcdefghijklmnopqrstuvwxyz';
 
     let passwordArray = [];
-
     // Generate random numerics
     for (let i = 0; i < numericCount; i++) {
         passwordArray.push(numerics[Math.floor(Math.random() * numerics.length)]);
     }
-
     // Generate random capital letters
     for (let i = 0; i < capitalCount; i++) {
         passwordArray.push(capitals[Math.floor(Math.random() * capitals.length)]);
     }
-
     // Generate random special characters
     for (let i = 0; i < specialCount; i++) {
         passwordArray.push(specials[Math.floor(Math.random() * specials.length)]);
     }
-
     // Generate random small letters
     for (let i = 0; i < smallCount; i++) {
         passwordArray.push(smalls[Math.floor(Math.random() * smalls.length)]);
     }
-
     // Shuffle the array to randomize the order of characters
     passwordArray.sort(() => Math.random() - 0.5);
 
